386ae558ccd64ef96eb88ee3906fb2320642dafc
TristanLikesUran <123839921+TristanLikesUran@users.noreply.github.com>
2024-10-15 18:28:43 +0200

    Update Config.cs
    Added Duration

--- Config.cs
+++ Config.cs
@@ -1,16 +1,15 @@
-﻿using Exiled.API.Interfaces;
+using Exiled.API.Interfaces;
 using PlayerRoles;
 using System.Collections.Generic;
 
-namespace SimpleTextChat
+public class Config : IConfig
 {
-    public class Config : IConfig
-    {
-        public bool IsEnabled { get; set; } = true;
-        public bool Debug { get; set; }
+    public bool IsEnabled { get; set; } = true;
+    public bool Debug { get; set; }
 
-        public List<RoleTypeId> BlacklistedProximityChat { get; set; } = new() { RoleTypeId.Scp079 };
-        public int MaxCharacters { get; set; } = 120;
-        public float ProximityRange { get; set; } = 8;
-    }
+    public List<RoleTypeId> BlacklistedProximityChat { get; set; } = new() { RoleTypeId.Scp079 };
+    public int MaxCharacters { get; set; } = 120;
+    public float ProximityRange { get; set; } = 8;
+
+    public float MessageDuration { get; set; } = 3;
 }
